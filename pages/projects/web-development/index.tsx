ece3437925fc6100592c0cc8d7a42e3f04cdb077
Shabd Saran <shabd.179301181@muj.manipal.edu>
2020-11-20 00:26:43 +0530

    Chore: Reorganized project pages file structure

---
+++ pages/projects/web-development/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+interface Props {}
+
+const WebDevelopmentProjects: React.FC<Props> = () => {
+  return <div className={styles['container']}></div>
+}
+
+export default WebDevelopmentProjects
