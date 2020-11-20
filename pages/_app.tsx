ee99c11ec757a5931f8dffc64a7a19e2bfc983ad
Shabd Saran <shabd.179301181@muj.manipal.edu>
2020-11-21 00:56:52 +0530

    Feat: Added default website title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 
 import 'pages/styles.scss'
 
@@ -8,7 +9,18 @@ interface Props {
 }
 
 const App: React.FC<Props> = ({ Component, pageProps }: Props) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Shabd Saran</title>
+        <link
+          rel='shortcut icon'
+          href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✌🏻</text></svg>'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
