4a1561d6ec96d285dece7af7f1aa8d3082804d16
Shabd Saran <shabd.179301181@muj.manipal.edu>
2020-11-21 23:03:28 +0530

    Feat: Added articles page

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
 
+import * as Card from 'components/helpers/card'
+import { PageTitle } from 'components/profile/PageTitle'
 import { NavbarButton } from 'components/helpers/navbar'
 
 import styles from './styles.module.scss'
@@ -10,7 +12,41 @@ const Articles: React.FC = () => {
     <div className={styles['outer-container']}>
       <Container className={styles['inner-container']}>
         <NavbarButton type='profile' />
+        <PageTitle>Articles</PageTitle>
+        <Card.Container>
+          <a
+            href='/articles/how-docker-can-help-you-sleep-well-at-night-507f1f77bcf86cd799439011'
+            className={styles['card-link']}
+          >
+            <Card.Item>
+              <Card.Title>
+                How Docker can help you sleep well at night?
+              </Card.Title>
+              <Card.Subtitles>
+                <Card.LeftSubtitle>DevOps with Docker</Card.LeftSubtitle>
+                <Card.RightSubtitle>Oct. 27, 2020</Card.RightSubtitle>
+              </Card.Subtitles>
+              <Card.Content>
+                Hello everyone, hope you're coding well. I was very surprised to
+                realize that a lot of junior developers, college students and
+                freelancers were not using Docker and some of them were not even
+                interested to learn it...
+              </Card.Content>
+            </Card.Item>
+          </a>
+        </Card.Container>
       </Container>
+
+      <style jsx global>
+        {`
+          body {
+            background: #f1c40f;
+            margin: 0;
+            padding: 0;
+            height: 100%;
+          }
+        `}
+      </style>
     </div>
   )
 }
