3b70d05b8e96e88090841cbbbf0c7db8e55e45e1
Shabd Saran <shabd.179301181@muj.manipal.edu>
2020-11-25 00:15:51 +0530

    Fix: Article metadata issue on /articles/[id]

--- pages/experiences/articles/[id]/index.tsx
+++ pages/experiences/articles/[id]/index.tsx
@@ -8,10 +8,18 @@ import { Container } from '@material-ui/core'
 
 import { BackButton } from 'components/profile/BackButton'
 
+import _data from 'static/articles/index.json'
+import { ArticleDataType } from 'utils/interfaces'
+
+const data = _data as ArticleDataType[]
+
 import styles from './styles.module.scss'
 
 const Article: React.FC = () => {
   const router = useRouter()
+  const [title, setTitle] = useState('')
+  const [series, setSeries] = useState('')
+  const [date, setDate] = useState('')
   const [markdown, setMarkdown] = useState('')
 
   useEffect(() => {
@@ -20,6 +28,9 @@ const Article: React.FC = () => {
     ;(async function () {
       const res = await axios.get(`/${id}.md`)
       setMarkdown(res.data)
+      setTitle(data.find((item) => item.code === id)?.title || '')
+      setSeries(data.find((item) => item.code === id)?.series || '')
+      setDate(data.find((item) => item.code === id)?.date || '')
     })()
   }, [router.query])
 
@@ -27,12 +38,10 @@ const Article: React.FC = () => {
     <div className={styles['outer-container']}>
       <Container className={styles['inner-container']}>
         <BackButton href='/experiences/articles' />
-        <h1 className={styles['title']}>
-          How Docker can help you sleep well at night?
-        </h1>
+        <h1 className={styles['title']}>{title}</h1>
         <div className={styles['metadata']}>
-          <div className={styles['series']}>DevOps with Docker</div>
-          <div className={styles['date']}>October 27, 2020</div>
+          <div className={styles['series']}>{series}</div>
+          <div className={styles['date']}>{date}</div>
         </div>
         <div
           className={styles['content']}
