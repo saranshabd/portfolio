ed7606b7ce49116ade11f241d39389cb46128ee1
Shabd Saran <shabd.179301181@muj.manipal.edu>
2021-04-14 22:15:48 +0530

    feat: removed all existing codebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,9 @@
 import React from 'react'
-import ReactFullpage from '@fullpage/react-fullpage'
-
-import { IntroProfile } from 'components/profile/intro'
-import { TechStackProfile } from 'components/profile/techStack'
-import { ProjectsProfile } from 'components/profile/projects'
-import { ExperiencesProfile } from 'components/profile/experiences'
-
-import { ReactFullPageSection } from 'components/helpers/ReactFullPageSection'
 
 const Home: React.FC = () => {
-  const anchors = [
-    {
-      name: 'intro',
-      Component: IntroProfile,
-    },
-    {
-      name: 'tech-stack',
-      Component: TechStackProfile,
-    },
-    {
-      name: 'experiences',
-      Component: ExperiencesProfile,
-    },
-    {
-      name: 'projects',
-      Component: ProjectsProfile,
-    },
-  ]
-
-  const renderSections = () => {
-    return (
-      <ReactFullpage.Wrapper>
-        {anchors.map(({ name, Component }) => (
-          <ReactFullPageSection
-            className={`${name}-section`}
-            component={Component}
-          />
-        ))}
-        <style jsx global>{`
-          #fp-nav ul li a span,
-          .fp-slidesNav ul li a span {
-            background: #fff;
-          }
-        `}</style>
-      </ReactFullpage.Wrapper>
-    )
-  }
 
   return (
-    <ReactFullpage
-      scrollingSpeed={1000}
-      anchors={anchors.map((anchor) => anchor.name)}
-      navigation={true}
-      render={renderSections}
-    />
+    <h1>Hello world</h1>
   )
 }
 
