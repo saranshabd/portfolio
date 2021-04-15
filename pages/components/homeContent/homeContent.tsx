1ec4a1a05800156140306a09c574b2d3c29bae0a
Shabd Saran <shabd.179301181@muj.manipal.edu>
2021-04-15 20:30:11 +0530

    feat: added writings screen

--- pages/components/homeContent/homeContent.tsx
+++ pages/components/homeContent/homeContent.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
 
 import styles from './homeContent.module.scss'
 
 interface Props {
   heading: string
   summary: string
+  url: string
 }
 
 const HomeContent: React.FC<Props> = (props: Props) => {
   return (
-    <div className={styles['content']}>
-      <div className={styles['heading']}>
-        <h2>{props.heading}</h2>
+    <Link href={props.url}>
+      <div className={styles['content']}>
+        <div className={styles['heading']}>
+          <h2>{props.heading}</h2>
+        </div>
+        <p>{props.summary}</p>
       </div>
-      <p>{props.summary}</p>
-    </div>
+    </Link>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,62 +5,61 @@ import styles from './home.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+import Container from './components/container/container'
 import HomeContent from './components/homeContent/homeContent'
 import Callout from './components/callout/callout'
 
 const Home: React.FC = () => {
   return (
-    <div className={styles['container']}>
-      <div className={styles['inner-container']}>
-        <h2>Shabd Saran</h2>
-        <p>
-          What I write makes absolutely no sense, so read it at your own risk
-        </p>
-        <div className={styles['social-media-links']}>
-          <a
-            href='https://twitter.com/saranshabd'
-            target='_blank'
-            className={`${styles['container']} ${styles['twitter-container']}`}
-          >
-            <FontAwesomeIcon
-              icon={faTwitter}
-              size='1x'
-              className={`${styles['icon']} ${styles['twitter-icon']}`}
-            />
-            <p>Twitter</p>
-          </a>
-          <a
-            href='https://www.linkedin.com/in/saranshabd'
-            target='_blank'
-            className={`${styles['container']} ${styles['linkedin-container']}`}
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              size='1x'
-              className={`${styles['icon']} ${styles['linkedin-icon']}`}
-            />
-            <p>LinkedIn</p>
-          </a>
-        </div>
-        <br />
-        <Callout>
-          If you are looking for my resume then head to my LinkedIn profile. I'd
-          recommend visiting my Twitter handle as well. Thanks :)
-        </Callout>
-        <br />
-        <div className={styles['divider']}>
-          <hr />
-        </div>
-        <h4 className={styles['title-container']}>Writings</h4>
-        <div>
-          <HomeContent
-            heading='With great power comes great responsibility'
-            summary='Having the knowledge of something and actually applying that knowledge
-        to do something valuable are two very different things.'
+    <Container isHomepage={true}>
+      <h2>Shabd Saran</h2>
+      <p>What I write makes absolutely no sense, so read it at your own risk</p>
+      <br />
+      <div className={styles['social-media-links']}>
+        <a
+          href='https://twitter.com/saranshabd'
+          target='_blank'
+          className={`${styles['container']} ${styles['twitter-container']}`}
+        >
+          <FontAwesomeIcon
+            icon={faTwitter}
+            size='1x'
+            className={`${styles['icon']} ${styles['twitter-icon']}`}
           />
-        </div>
+          <p>Twitter</p>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/saranshabd'
+          target='_blank'
+          className={`${styles['container']} ${styles['linkedin-container']}`}
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size='1x'
+            className={`${styles['icon']} ${styles['linkedin-icon']}`}
+          />
+          <p>LinkedIn</p>
+        </a>
       </div>
-    </div>
+      <br />
+      <Callout>
+        If you are looking for my resume then head to my LinkedIn profile. I'd
+        recommend visiting my Twitter handle as well. Thanks :)
+      </Callout>
+      <br />
+      <div className={styles['divider']}>
+        <hr />
+      </div>
+      <h4 className={styles['title-container']}>Writings</h4>
+      <div>
+        <HomeContent
+          heading='With great power comes great responsibility'
+          summary='Having the knowledge of something and actually applying that knowledge
+        to do something valuable are two very different things.'
+          url='/writings/with-great-power-comes-great-responsibility'
+        />
+      </div>
+    </Container>
   )
 }
 
