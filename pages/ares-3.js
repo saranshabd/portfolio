e0a9b6ab8c16cb1bb41af7178d2c20bc38d31389
Shabd Saran <saranshabd@gmail.com>
2022-10-27 21:40:30 +0530

    feat: added substack articles

--- pages/ares-3.js
+++ pages/ares-3.js
@@ -1,7 +1,11 @@
-import BaseLayout from "../components/BaseLayout";
-import BlogHeader from "../components/BlogHeader";
+import BaseLayout from '../components/BaseLayout'
+import BlogHeader from '../components/BlogHeader'
 
 function Ares3() {
+  function getURL(slug) {
+    return `https://open.substack.com/pub/saranshabd/p/${slug}?utm_campaign=portfolio&utm_medium=web`
+  }
+
   return (
     <BaseLayout>
       <h2>Ares 3</h2>
@@ -12,12 +16,22 @@ function Ares3() {
       </p>
       <br />
       <BlogHeader
+        title="Create virtual identity within minutes"
+        publishedOn="26th October 2022"
+        href={getURL('create-virtual-identity-within-minutes')}
+      />
+      <BlogHeader
+        title="How to finish any book in a single day?"
+        publishedOn="12th September 2022"
+        href={getURL('how-to-finish-any-book-in-a-single')}
+      />
+      <BlogHeader
         title="LOG ENTRY: SOL 1"
         publishedOn="28th August 2022"
-        href="https://saranshabd.substack.com/p/log-entry-sol-1"
+        href={getURL('log-entry-sol-1')}
       />
     </BaseLayout>
-  );
+  )
 }
 
-export default Ares3;
+export default Ares3
