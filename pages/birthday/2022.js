98d3edbb13ebb5555f301966f7f77f419f53e5c5
Shabd Saran <saranshabd@gmail.com>
2022-11-03 23:47:58 +0530

    mod: updated the birthday page

--- pages/birthday/2022.js
+++ pages/birthday/2022.js
@@ -3,6 +3,7 @@ import BaseLayout from '../../components/BaseLayout'
 function Birthday() {
   return (
     <BaseLayout header={false} footer={false}>
+      <p>On the occasion of my 23rd birthday</p>
       <h3>Go fuck yourself! 🎉</h3>
       <img
         // src="https://media.tenor.com/bxp_GSuKsmsAAAAd/fuck-yeah-fu.gif"
