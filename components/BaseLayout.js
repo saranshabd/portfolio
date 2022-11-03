bf8c26aece1b46819dd350f550c57f397cca37ca
Shabd Saran <saranshabd@gmail.com>
2022-11-03 21:15:23 +0530

    feat: added birthday 2022 page

--- components/BaseLayout.js
+++ components/BaseLayout.js
@@ -1,68 +1,72 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-import DefaultHead from "./DefaultHead";
-import Link from "./Link";
-import Footer from "./Footer";
+import DefaultHead from './DefaultHead'
+import Link from './Link'
+import Footer from './Footer'
 
-import styles from "../styles/Home.module.css";
+import styles from '../styles/Home.module.css'
 
-function BaseLayout({ children }) {
+function BaseLayout({ children, header = true, footer = true }) {
   return (
     <div className={styles.container}>
       <DefaultHead />
 
       <main>
-        <div className={styles.header}>
-          {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-          <a className={styles.pageTitleHeader} href="/">
-            <h1>Shabd Saran</h1>
-          </a>
-          <div className={styles.socialLinks}>
-            <Link href="https://linkedin.com/in/saranshabd">
-              <Image
-                height="25rem"
-                width="25rem"
-                src="/LinkedIn.svg"
-                alt="LinkedIn"
-              />
-            </Link>
-            <Link href="https://twitter.com/saranshabd">
-              <Image
-                height="25rem"
-                width="25rem"
-                src="/Twitter.svg"
-                alt="Twitter"
-              />
-            </Link>
-            <Link href="https://github.com/saranshabd">
-              <Image
-                height="25rem"
-                width="25rem"
-                src="/GitHub.svg"
-                alt="GitHub"
-              />
-            </Link>
-            <Link href="https://saranshabd.substack.com">
-              <Image
-                height="25rem"
-                width="25rem"
-                src="/Substack.svg"
-                alt="Substack"
-              />
-            </Link>
-          </div>
-        </div>
+        {header && (
+          <>
+            <div className={styles.header}>
+              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+              <a className={styles.pageTitleHeader} href="/">
+                <h1>Shabd Saran</h1>
+              </a>
+              <div className={styles.socialLinks}>
+                <Link href="https://linkedin.com/in/saranshabd">
+                  <Image
+                    height="25rem"
+                    width="25rem"
+                    src="/LinkedIn.svg"
+                    alt="LinkedIn"
+                  />
+                </Link>
+                <Link href="https://twitter.com/saranshabd">
+                  <Image
+                    height="25rem"
+                    width="25rem"
+                    src="/Twitter.svg"
+                    alt="Twitter"
+                  />
+                </Link>
+                <Link href="https://github.com/saranshabd">
+                  <Image
+                    height="25rem"
+                    width="25rem"
+                    src="/GitHub.svg"
+                    alt="GitHub"
+                  />
+                </Link>
+                <Link href="https://saranshabd.substack.com">
+                  <Image
+                    height="25rem"
+                    width="25rem"
+                    src="/Substack.svg"
+                    alt="Substack"
+                  />
+                </Link>
+              </div>
+            </div>
 
-        <br />
+            <br />
+          </>
+        )}
 
         {children}
 
-        <br />
+        {footer && <br />}
       </main>
 
-      <Footer />
+      {footer && <Footer />}
     </div>
-  );
+  )
 }
 
-export default BaseLayout;
+export default BaseLayout
---
+++ pages/birthday/2022.js
@@ -0,0 +1,19 @@
+import BaseLayout from '../../components/BaseLayout'
+
+function Birthday() {
+  return (
+    <BaseLayout header={false} footer={false}>
+      <h3>Go fuck yourself! 🎉</h3>
+      <img
+        // src="https://media.tenor.com/bxp_GSuKsmsAAAAd/fuck-yeah-fu.gif"
+        src="/fuck-you.gif"
+        width="100%"
+      />
+      <br />
+      <br />
+      <h4>Regards, Shabd :)</h4>
+    </BaseLayout>
+  )
+}
+
+export default Birthday
