3522afd800c7d1614b81e7ad1f2c01b6714196cd
Shabd Saran <saranshabd@gmail.com>
2023-08-02 12:26:36 +0530

    feat: added Contextualising identifiers in a system article

--- pages/technical-blogs.js
+++ pages/technical-blogs.js
@@ -12,6 +12,11 @@ function TechnicalBlogs() {
       </p>
       <br />
       <BlogHeader
+        title="Contextualising identifiers in a system"
+        publishedOn="2nd August 2023"
+        href="https://dev.to/saranshabd/contextualising-identifiers-in-a-system-4mm6"
+      />
+      <BlogHeader
         title="Go migrate from-express"
         publishedOn="30th October 2022"
         href="https://dev.to/saranshabd/go-migrate-from-express-2amm"
